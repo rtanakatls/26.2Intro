77fb1f678eff4d1fb1255c74cedac2b731852fd9
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-09-02 11:57:04 -0500

    Created project

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro262
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Hola, introduce tu nombre:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hey te llamas {name}");
+            Console.WriteLine("Introduce tu edad:");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Veo que tienes {age} años y el próximo cumplirás {age + 1}");
+
+
+        }
+    }
+}

867ae961e9e7dde1bad9142573c09c0c4cd5cc61
Ricardo Tanaka Terukina <rtanaka@talento.tls.edu.pe>
2026-09-02 12:30:23 -0500

    Added examples

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Intro262
     {
         static void Main(string[] args)
         {
+            /*
             string name;
             int age;
 
@@ -19,6 +20,33 @@ namespace Intro262
             Console.WriteLine("Introduce tu edad:");
             age = int.Parse(Console.ReadLine());
             Console.WriteLine($"Veo que tienes {age} años y el próximo cumplirás {age + 1}");
+            */
+
+            float a;
+            float b;
+
+            a = 10;
+            b = 20;
+
+            Console.WriteLine($"La suma es {a + b}");
+            Console.WriteLine($"La resta es {a - b}");
+            Console.WriteLine($"La multiplicación es {a * b}");
+
+            Console.WriteLine($"La división es {a / b}");
+            Console.WriteLine($"Él módulo es {a % b}");
+
+
+            Console.WriteLine($"El área del rectángulo es {a * b} y el perímetro es {2 * (a + b)}");
+
+            float days = 1000;
+
+            Console.WriteLine($"{days} días en años es {days / 365} años");
+            Console.WriteLine($"{days} días en semanas es {days / 7} semanas");
+
+            float angle1 = 90;
+            float angle2 = 60;
+
+            Console.WriteLine($"El tercer ángulo vale {180 - (angle1 + angle2)}");
 
 
         }
